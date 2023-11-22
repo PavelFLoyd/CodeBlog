c1e5926f77e8e46e20cf94d9fbbf2f25de642093
p1nkf1oyd <ubpavelub@mail.ru>
2023-11-22 14:45:03 +0300

    First card ready
    Сделана шаблонная карточка статьи на главной странице

--- Frontend/src/app/article-list/article-list.component.ts
+++ Frontend/src/app/article-list/article-list.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-article-list',
   templateUrl: './article-list.component.html',
-  styleUrls: ['./article-list.component.scss']
+  styleUrls: ['./article-list.component.scss'],
 })
 export class ArticleListComponent {
-
+  commentsvg: string = '/assets/commentsvg.svg';
 }
