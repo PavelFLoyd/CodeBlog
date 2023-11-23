021011ae7a62ff004f0014c4f41daa4e78d11504
p1nkf1oyd <ubpavelub@mail.ru>
2023-11-23 17:23:21 +0300

    working on admin panel

--- Frontend/src/app/admin-panel/admin-panel.component.ts
+++ Frontend/src/app/admin-panel/admin-panel.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-admin-panel',
   templateUrl: './admin-panel.component.html',
   styleUrls: ['./admin-panel.component.scss'],
 })
-export class AdminPanelComponent {}
+export class AdminPanelComponent {
+  dropsvg: string = '/assets/addphoto.svg';
+  @ViewChild('fileInput') fileInput!: ElementRef;
+
+  handleFileInputClick(): void {
+    this.fileInput.nativeElement.click();
+  }
+
+  handleFileInputChange(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      console.log('Выбранный файл:', file);
+    }
+  }
+}
--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DD Code | Blog';
+  isAdminPanel: boolean = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isAdminPanel = this.router.url.includes('/Admin');
+      }
+    });
+  }
 }
